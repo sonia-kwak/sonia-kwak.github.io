b252173bf0616bdcd12db4540d081ddde259c3a5
Cristiano Dalbem <ninja.dalbem@gmail.com>
2018-11-24 17:44:33 -0200

    Create icons component

---
+++ src/components/icons.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const leftArrow = (
+    <svg width='13' height='8' viewBox='0 0 26 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <path d='M26 8H1' stroke='currentColor' />
+        <path d='M8 15L1 8L8 1' stroke='currentColor' />
+    </svg>
+);
+
+export const rightArrow = (
+    <svg width='13' height='8' viewBox='0 0 26 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <path d='M25 8L25.7071 8.70711C26.0976 8.31658 26.0976 7.68342 25.7071 7.29289L25 8ZM0 9H25V7H0V9ZM17.2929 1.70711L24.2929 8.70711L25.7071 7.29289L18.7071 0.292893L17.2929 1.70711ZM24.2929 7.29289L17.2929 14.2929L18.7071 15.7071L25.7071 8.70711L24.2929 7.29289Z'
+        fill='currentColor' />
+    </svg>
+);
+
+export const upAndLeftArrow = (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'
+        fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round'
+        strokeLinejoin='round' className='feather feather-corner-left-up'>
+        <polyline points='14 9 9 4 4 9' />
+        <path d='M20 20h-7a4 4 0 0 1-4-4V4' />
+    </svg>
+);
