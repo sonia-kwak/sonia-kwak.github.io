77c4b10f74f04c46660b15537fd6467871256265
Cristiano Dalbem <ninja.dalbem@gmail.com>
2021-06-07 14:25:56 -0300

    Revamp social section

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,29 +5,33 @@ import LinksList from './LinksList'
 const Social = () => {
   const items = [
     {
-      label: 'LinkedIn',
-      url: "https://www.linkedin.com/in/cmdalbem/"
-    },
-    {
-      label: 'Email',
-      url: "mailto:cristiano.dalbem@gmail.com"
-    },
-    {
       label: 'Twitter',
       url: "https://twitter.com/cmdalbem"
+    },
+    {
+      label: 'LinkedIn',
+      url: "https://www.linkedin.com/in/cmdalbem/"
     },
     {
       label: 'GitHub',
       url: "https://github.com/cmdalbem/"
     },
     {
-      label: 'Goodreads (books)',
-      url: "https://www.goodreads.com/cmdalbem"
+      label: 'Dribbble',
+      url: "https://dribbble.com/cmdalbem/"
     },
     {
-      label: 'Letterboxd (movies)',
-      url: "https://letterboxd.com/cmdalbem/"
-    }
+      label: 'Email',
+      url: "mailto:cristiano.dalbem@gmail.com"
+    },
+    // {
+    //   label: 'Goodreads (books)',
+    //   url: "https://www.goodreads.com/cmdalbem"
+    // },
+    // {
+    //   label: 'Letterboxd (movies)',
+    //   url: "https://letterboxd.com/cmdalbem/"
+    // }
   ];
 
   return (
