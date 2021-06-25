1b10aa6fa75f111ca92d01fdfb90ddcd6b156345
Cristiano Dalbem <ninja.dalbem@gmail.com>
2021-06-24 23:28:22 -0300

    Refactor markdown components and little improvements

---
+++ src/components/markdownComponents.js
@@ -0,0 +1,133 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade';
+import rehypeReact from "rehype-react"
+
+import LinksList from '../components/LinksList'
+
+
+class ResultsBanner extends React.Component {
+    static defaultProps = {
+        data: {},
+    }
+
+    render() {
+        const dataObj = JSON.parse(this.props.data);
+
+        return (
+            <div
+                className="nl6-ns nr6-ns nl4-m nr4-m nl3 nr3 mv6 pa6-ns pv6 bg-near-white flex flex-row-l flex-column justify-around tl-ns tc"
+            >
+                {
+                    Object.keys(dataObj).map((i, n) => (
+                        <Fade bottom duration={1500} delay={n * 500}>
+                            <div className="mv0-ns mv3" key={i}>
+                                <div className="f1 fw1 mt0">
+                                    {dataObj[i]}
+                                </div>
+                                <div className="f7 gray ttu mb0 tracked">
+                                    {i}
+                                </div>
+                            </div>
+                        </Fade>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export function markdownRenderer(isFullWidth) {
+    const defaultMargins = `center ${isFullWidth ? 'layoutMaxWidth' : 'mw7'}`;
+    const bigImageMargins = `nl5-ns nr5-ns nl3-m nr3-m mv6-ns mv5 `;
+    const imageMargins = `${defaultMargins} mv6-ns mv5 `;
+
+    return new rehypeReact({
+        createElement: React.createElement,
+        components: {
+            h1: props => (
+                <h1 className={`f2 lh-title dark-gray fw4 mt6-ns mt5 mb4 ${defaultMargins}`}>
+                    {props.children}
+                </h1>
+            ),
+            h2: props => (
+                <h2 className={`f3 lh-title dark-gray fw4 mt5-ns mt4 mb3-ns mb2 ${defaultMargins}`}>
+                    {props.children}
+                </h2>
+            ),
+            h3: props => (
+                <h3 className={`f4-ns f5 lh-title gray fw6 mt4 ${defaultMargins}`}>
+                    {props.children}
+                </h3>
+            ),
+            p: props => (
+                <p className={`mt0 ${defaultMargins}`}>
+                    {props.children
+                    }</p>
+            ),
+            blockquote: props => (
+                <div className={`mt0 pl4 mv5 f3 gray ${defaultMargins}`} style={{ textIndent: '-.5em' }}>
+                    {props.children}
+                </div>
+            ),
+            ul: props => (
+                <ul className={`${defaultMargins}`}>
+                    {props.children}
+                </ul>
+            ),
+            li: props => (
+                <li className="mb2">
+                    {props.children}
+                </li>
+            ),
+            a: props => (
+                <a
+                    href={props.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="pretty-link"
+                >
+                    {props.children}
+                </a>
+            ),
+            hr: props => (
+                <hr className="mv6 bt-0 bb b--black-10" />
+            ),
+            figure: props => (
+                <figure className={imageMargins}>{props.children}</figure>
+            ),
+            figcaption: props => (
+                <figcaption className="mt2 f6 dark-gray">{props.children}</figcaption>
+            ),
+            code: props => (
+                <code className="f6 bg-light-gray ph2">{props.children}</code>
+            ),
+            "jumbo": props => (
+                <div className={bigImageMargins}>
+                    {props.children}
+                    {
+                        props.caption &&
+                        <figcaption className={defaultMargins + " mt4 fw6 f6 dark-gray tc"}>
+                            {props.caption}
+                        </figcaption>
+                    }
+                </div>
+            ),
+            "video-container": props => (
+                <div className={imageMargins}>
+                    {props.children}
+                </div>
+            ),
+            "results-banner": ResultsBanner,
+            "links-list": props => {
+                let arrayObj = JSON.parse(props.items);
+                let items = arrayObj.map(i => i);
+
+                return (
+                    <p className={`mt0 ${defaultMargins}`}>
+                        <LinksList items={items} rows />
+                    </p>
+                )
+            }
+        },
+    }).Compiler;
+}
--- src/templates/ProjectPage.js
+++ src/templates/ProjectPage.js
@@ -9,48 +9,16 @@ import Fade from 'react-reveal/Fade';
 
 import { BrowserView } from 'react-device-detect';
 
-import rehypeReact from "rehype-react"
-
 import Tag from '../components/Tag'
 import Layout from '../components/Layout'
 import ProjectCard from '../components/ProjectCard'
-import LinksList from '../components/LinksList'
 import ReadingProgressBar from '../components/ReadingProgressBar'
 
+import { markdownRenderer }  from '../components/markdownComponents'
 import { formatDate, capitalize } from '../components/utils.js'
  
 // import { RightArrow, LeftArrow } from '../components/icons.js'
 
-class ResultsBanner extends React.Component {
-  static defaultProps = {
-    data: {},
-  }
-
-  render() {
-    const dataObj = JSON.parse(this.props.data);
-
-    return (
-      <div 
-        className="nl6-ns nr6-ns nl4-m nr4-m nl3 nr3 mv6 pa6-ns pv6 bg-near-white flex flex-row-l flex-column justify-around tl-ns tc"
-      >
-        {
-          Object.keys(dataObj).map ( (i, n) => (
-            <Fade bottom duration={1500} delay={n*500}>
-              <div className="mv0-ns mv3" key={i}>
-                <div className="f1 fw1 mt0">
-                  {dataObj[i]}
-                </div>
-                <div className="f7 gray ttu mb0 tracked">
-                  {i}
-                </div>
-              </div>
-            </Fade>
-          ))
-        }
-      </div>
-    )
-  }
-}
 
 class ProjectPage extends React.Component {
   render() {
@@ -58,9 +26,6 @@ class ProjectPage extends React.Component {
     const { previous, next } = this.props.pageContext
     const readingTime = post.fields.readingTime; 
 
-    const defaultMargins = `center ${post.frontmatter.fullWidth ? 'layoutMaxWidth' : 'mw7'}`;
-    const bigImageMargins = `nl5-ns nr5-ns nl3-m nr3-m mv6-ns mv5 `;
-    const imageMargins = `${defaultMargins} mv6-ns mv5 `;
     const baseType = 'f4-ns f5 dark-gray lh-copy ';
 
     let dateStart = formatDate(post.frontmatter.date, 'MMMM YYYY');
@@ -69,96 +34,6 @@ class ProjectPage extends React.Component {
       dateEnd = null;
     }
 
-    const renderAst = new rehypeReact({
-      createElement: React.createElement,
-      components: {
-        h1: props => (
-          <h1 className={`f2 lh-title dark-gray fw4 mt6-ns mt5 mb4 ${defaultMargins}`}>
-            {props.children}
-          </h1>
-        ), 
-        h2: props => (
-          <h2 className={`f3 lh-title dark-gray fw4 mt5-ns mt4 mb3-ns mb2 ${defaultMargins}`}>
-            {props.children}
-          </h2> 
-        ),
-        h3: props => (
-          <h3 className={`f4-ns f5 lh-title gray fw6 mt4 ${defaultMargins}`}>
-            {props.children}
-          </h3>
-        ),
-        p: props => (
-          <p className={`mt0 ${defaultMargins}`}>
-            {props.children
-          }</p>
-        ),
-        blockquote: props => (
-          <div className={`mt0 pl4 mv5 f3 gray ${defaultMargins}`} style={{textIndent: '-.5em'}}>
-            {props.children}
-          </div>
-        ),
-        ul: props => (
-          <ul className={`${defaultMargins}`}>
-            {props.children}
-          </ul>
-        ),
-        li: props => (
-          <li className="mb2">
-            {props.children}
-          </li>
-        ),
-        a: props => (
-          <a
-            href={props.href}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="pretty-link"
-          >
-            {props.children}
-          </a>
-        ), 
-        hr: props => (
-          <hr className="mv6 bt-0 bb b--black-10" />
-        ),
-        figure: props => (
-          <figure className={imageMargins}>{props.children}</figure>
-        ),
-        figcaption: props => (
-          <figcaption className="mt4 fw6 f6 dark-gray tc">{props.children}</figcaption>
-        ),
-        code: props => (
-          <code className="f6 bg-light-gray ph2">{props.children}</code>
-        ),
-        "jumbo": props => (
-          <div className={bigImageMargins}>
-            { props.children }
-            {
-              props.caption &&
-              <figcaption className={defaultMargins + " mt4 fw6 f6 dark-gray tc"}>
-                {props.caption}
-              </figcaption>
-            }
-          </div>
-        ),
-        "video-container": props => (
-          <div className={imageMargins}> 
-            {props.children}
-          </div>
-        ),
-        "results-banner": ResultsBanner,
-        "links-list": props => {
-          let arrayObj = JSON.parse(props.items);
-          let items = arrayObj.map( i => i);
-          
-          return (
-            <p className={`mt0 ${defaultMargins}`}>
-              <LinksList items={items} rows/>
-            </p>
-          )
-        }
-      },
-    }).Compiler;
-    
     return (
       <Layout location={this.props.location}>
         <Helmet>
@@ -222,15 +97,28 @@ class ProjectPage extends React.Component {
 
               <div className="w-30-ns">
                 {
+                  <div className='mv4 dark-gray'>
+                    <h2 className="f6 fw6 ttu mv2 fw7 mr2">
+                      <span>
+                        Challenge
+                      </span>
+                    </h2>
+                    <span className="f5 din lh-copy">
+                      {post.frontmatter.minibio}
+                    </span>
+                  </div>
+                }
+
+                {
                   post.frontmatter.tags &&
                   post.frontmatter.tags.length > 0 &&
                   <div className="mb4 dark-gray">
                     <h2 className="f6 fw6 ttu mv2 fw7 mr2">
-                      <span className="">
+                      <span>
                         Roles
                       </span>
                     </h2>
-                    <div className="">
+                    <div>
                       { post.frontmatter.tags.map(t => capitalize(t)).join(', ') }
                       {/* {post.frontmatter.tags.map(tag => (
                         <Tag size="big" key={tag}>
@@ -245,7 +133,7 @@ class ProjectPage extends React.Component {
                   post.frontmatter.team &&
                   <div className='mv4 dark-gray'>
                     <h2 className="f6 fw6 ttu mv2 fw7 mr2">
-                      <span className="">
+                      <span>
                         Team
                       </span>
                     </h2>
@@ -258,7 +146,7 @@ class ProjectPage extends React.Component {
                 {
                   <div className="mv4 dark-gray">
                     <h2 className="f6 fw6 ttu mv2 fw7 mr2">
-                      <span className="">
+                      <span>
                         Date
                       </span>
                     </h2>
@@ -283,7 +171,7 @@ class ProjectPage extends React.Component {
                   // </div>
                   <a
                     href={post.frontmatter.liveLink} target="_blank" rel="noopener noreferrer"
-                    className="dib f5 fw6 link pv3 ph5 ba br1"
+                    className="dib f5 fw6 link pv3 ph5 ba br1 dim"
                     style={{color: post.frontmatter.color}}
                   >
                     See it live
@@ -297,7 +185,7 @@ class ProjectPage extends React.Component {
         {/* Content */}
         <div className="flex flex-column">
           <div className={baseType}>
-            { renderAst(post.htmlAst) }
+            { markdownRenderer(post.frontmatter.fullWidth)(post.htmlAst) }
           </div>
         </div> 
 
@@ -336,7 +224,7 @@ class ProjectPage extends React.Component {
           </div>
 
           <div className="w-100 tc mt6">
-            <Link to="/#case studies" className="gradient-border text-gradient-clip bg-gradient dim f4 fw6 link pa3">
+            <Link to="/#case studies" className="gradient-border text-gradient-clip bg-gradient dim f4 fw6 link pa3 br1">
               View all projects
             </Link>
           </div>
@@ -358,6 +246,7 @@ export const pageQuery = graphql`
              frontmatter {
                title
                description
+               minibio
                date
                date2
                liveLink
